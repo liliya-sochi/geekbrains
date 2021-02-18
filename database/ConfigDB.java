ee07046b82084e6f10537edca77221f0b53f6dea
liliya.sochi <liliya.sochi@mail.ru>
2021-02-18 21:35:55 +0300

    DataBase&Animations

---
+++ database/ConfigDB.java
@@ -0,0 +1,9 @@
+package database;
+
+public class ConfigDB {
+    protected String dbHost = "localhost";
+    protected String dbPort= "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "FromSochi";
+    protected String dbName = "mychat";
+}
---
+++ database/DBHandler.java
@@ -0,0 +1,68 @@
+package database;
+
+import main.User;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.sql.SQLIntegrityConstraintViolationException;
+
+public class DBHandler extends ConfigDB {
+    Connection dbConnection;
+
+    public Connection getDbConnection() throws ClassNotFoundException, SQLException {
+        String connectionString = "jdbc:mysql://" + dbHost + ":" + dbPort + "/" + dbName;
+        Class.forName("com.mysql.jdbc.Driver");
+        dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPass);
+        return dbConnection;
+    }
+
+    public void singUpUser(User user) throws SQLIntegrityConstraintViolationException, SQLException  {
+        String insert = "INSERT INTO " + ConstDB.USER_TABLE + "(" + ConstDB.USERS_NAME + "," + ConstDB.USERS_LOGIN +
+                "," + ConstDB.USERS_PASSWORD + ")" + "VALUES(?,?,?)";
+        try {
+            PreparedStatement preparedStatement = getDbConnection().prepareStatement(insert);
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getLogin());
+            preparedStatement.setString(3, user.getPassword());
+            preparedStatement.executeUpdate();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ResultSet getUser(User user) {
+        ResultSet resultSet = null;
+        String select = "SELECT * FROM " + ConstDB.USER_TABLE + " WHERE " + ConstDB.USERS_LOGIN + "=? AND " +
+                ConstDB.USERS_PASSWORD + "=?";
+        try {
+            PreparedStatement preparedStatement = getDbConnection().prepareStatement(select);
+            preparedStatement.setString(1, user.getLogin());
+            preparedStatement.setString(2, user.getPassword());
+            resultSet = preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public ResultSet findLogin(User user) {
+        ResultSet resultSet = null;
+        String select = "SELECT * FROM " + ConstDB.USER_TABLE + " WHERE " + ConstDB.USERS_LOGIN + "=?";
+        try {
+            PreparedStatement preparedStatement = getDbConnection().prepareStatement(select);
+            preparedStatement.setString(1, user.getLogin());
+            resultSet = preparedStatement.executeQuery();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+}
