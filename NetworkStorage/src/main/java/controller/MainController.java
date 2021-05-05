00b5675162eaf373742bd3e3301c0b9156169e30
liliya.sochi <liliya.sochi@mail.ru>
2021-05-06 00:47:19 +0300

    Update Project

---
+++ NetworkStorage/src/main/java/controller/MainController.java
@@ -0,0 +1,35 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class MainController {
+
+    @FXML
+    private AnchorPane main;
+
+    @FXML
+    private TextField texConsole;
+
+    @FXML
+    private Button butConsole;
+
+    @FXML
+    private ImageView icoExit;
+
+    @FXML
+    private ImageView icoAddFolder;
+
+    @FXML
+    private ImageView icoDelFolder;
+
+    @FXML
+    private ImageView icoAddFile;
+
+    @FXML
+    private ImageView icoDelFile;
+
+}
---
+++ NetworkStorage/src/main/java/controller/SingUpController.java
@@ -0,0 +1,30 @@
+package controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class SingUpController {
+
+    @FXML
+    private AnchorPane main;
+
+    @FXML
+    private TextField texName;
+
+    @FXML
+    private TextField texLogin;
+
+    @FXML
+    private PasswordField pasPassword;
+
+    @FXML
+    private Button butSingUp;
+
+    @FXML
+    private Label labWarning;
+
+}
